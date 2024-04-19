d969c50e31ac9d63e639ee2721895f4f1548de69
Jishnu Chakraborty <156436161+JishnuC24@users.noreply.github.com>
2024-04-19 13:27:58 -0400

    Update backend.tf

--- project/terraform/backend.tf
+++ project/terraform/backend.tf
@@ -4,7 +4,7 @@ terraform {
     storage_account_name = "tfstaten01553065sa"
     container_name       = "tfstatefiles"
     key                  = "terraform.tfstate"
-    access_key           = "iS5BDainHUIFqetpliFxYd97F2hbJDurPrX9v6CF1iasftXETsv5vZECZtd+whLcoR2z7qI0QLPT+AStBszHYQ=="
+    access_key           = ""
   }
 }
 
